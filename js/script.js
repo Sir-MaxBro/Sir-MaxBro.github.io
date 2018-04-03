9438d5ce7352934f91dc287c5302fcbf775f7d68
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-03 15:38:39 +0300

    Add body .load

--- js/script.js
+++ js/script.js
@@ -16,9 +16,10 @@ function slideMenu() {
 
 document.body.onload = function() {
     setTimeout(function() {
-      var preloader = document.getElementById('page-preloader');
-      if (!preloader.classList.contains('done')) {
-        preloader.classList.add('done');
-      }
+        var preloader = document.getElementById('page-preloader');
+        if (!preloader.classList.contains('done')) {
+            preloader.classList.add('done');
+        }
     }, 500);
-  };
+    docyment.body.classList.add('load');
+};
