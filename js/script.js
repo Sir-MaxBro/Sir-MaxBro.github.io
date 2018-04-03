fcda856a2e065e35911d0b98f841538c2d99c9bd
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-03 15:31:48 +0300

    Add Dnepr-Elevator site

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function initMap() {
+    var center = { lat: 53.906189, lng: 30.336034 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: center
+    });
+    var marker = new google.maps.Marker({
+        position: center,
+        map: map
+    });
+}
+
+function slideMenu() {
+    $('#menu').slideToggle();
+}
+
+document.body.onload = function() {
+    setTimeout(function() {
+      var preloader = document.getElementById('page-preloader');
+      if (!preloader.classList.contains('done')) {
+        preloader.classList.add('done');
+      }
+    }, 500);
+  };
